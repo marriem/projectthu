470fcd37b11e5542ca9812ccdeaa46e47aa0a9f6
Maryam <mariamly98@yahoo.com>
2021-03-21 00:43:45 +0300

    update

--- src/component/footer.js
+++ src/component/footer.js
@@ -6,11 +6,11 @@
             return (
             
             <div className="container navbar-fixed-bottom">
-        <div className=" text-center ">
+        <div className=" text-center navbar-dark ">
           <p>
             &copy;2021 TEDxHelwanUniversity. This independent TEDx event is operated under license from TED.{" "}
           </p>
-          <div className="social-fot">
+          <div className="social-fot container-fluid">
           <ul className="displayi">
             <li className="ico">
               <a target="_blank" rel="noreferrer" href={'https://www.linkedin.com/company/tedxhuni/'}
