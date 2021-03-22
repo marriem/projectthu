73e780af24b520826d351d475981d404be62c92e
Maryam <mariamly98@yahoo.com>
2021-03-23 01:18:15 +0300

    update3.2

--- src/component/header.js
+++ src/component/header.js
@@ -2,59 +2,70 @@
 import React, { Component } from "react";
 // import {Home, Attend, Team , Watch, Contactus} from "../screens/index";
 import logob from '../logob.png';
-// import { Home } from "../screens";
+import home from "../screens/home";
 
 export class Header extends Component {
 
   render() {
     
     return (
-      <nav id="menu" className="navbar-default navbar-fixed-top navbar-dark " >
+      <nav id="menu" className="navbar-default navbar-fixed-top navbar-dark " role="navigation">
         <div className="container">
 
           <div className="navbar-header naaz">
-            <button
-              type="button"
-              className="navbar-toggle collapsed"
-              data-toggle="collapse"
-              data-target="#bs-example-navbar-collapse-1"/>
+          <button
+          type="button"
+          className="navbar-toggle collapsed"
+          data-toggle="collapse"
+          data-target=".navbar-collapse"
+        >
+          {" "}
+          <span className="sr-only">Toggle navigation</span>{" "}
+          <span className="icon-bar"></span>{" "}
+          <span className="icon-bar"></span>{" "}
+          <span className="icon-bar"></span>{" "}
+        </button>
            
             <img alt="logo" prop="logo" src={logob} style={{width:250,hieght:250,alignSelf:"center",marginTop:10}}/>
           {" "}
           </div>
-          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <div className="navbar-collapse collapse" id="navbar-collapse">
 
 
             <ul className="nav navbar-nav navbar-left">
-              <li>
-                <a href="./" className="page-scroll">
+              <li >
+                <a href="#" className="page-scroll">
                   Home
                   </a>
               </li>
               <li>
-                <a href="./about" className="page-scroll">
+                <a href="index.html" className="page-scroll">
                   Attend
                   </a>
               </li>
-              <li>
-                <a href="#services" className="page-scroll">
-                  Watch
-                  </a>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b class="caret"></b> Watch</a>
+                <ul class="dropdown-menu">
+                  <li class="dropdown-header"></li>
+                <li><a href="#">Event 1 </a></li>
+                <li><a href="#">Event 2 </a></li>
+                <li><a href="#">Event 3 </a></li>
+                </ul>
               </li>
             </ul>
             <ul className="nav navbar-nav navbar-right">
               <li>
-                <a href="#about" className="page-scroll">
+                <a href="index.html" className="page-scroll">
                   About
                   </a>
               </li>
               <li>
-                <a href="#team" className="page-scroll">
+                <a href="index.html" className="page-scroll">
                   Sponsors
                   </a>
               </li>
               <li>
-                <a href="#contact" className="page-scroll">
+                <a href="index.html" className="page-scroll">
                   Contact us
                   </a>
               </li>
--- src/screens/home.js
+++ src/screens/home.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
-import {Header, Footer} from '../component/index';
+import {Header, Footer,About} from '../component/index';
 
 export class home extends Component {
   render() {
     return (
-        <div className="container">
+        <div id="Home" className="container">
           <div><Header></Header></div>
-<div>
+          <About></About>
+          <div className="brief-conatiner "></div>
+<div className="brief-conatiner ">
   <text> HEMMLDLDLFLF</text>
 </div>
 <Footer></Footer>
